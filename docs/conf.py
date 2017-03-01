6943fe9a7cf3399785e225ee7794516b71bb5109
Pete Woyzbun <peter.woyzbun@gmail.com>
2017-03-01 14:20:44 -0500

    Initializing docs.

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,8 @@ import os
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
+import sphinxcontrib.napoleon
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
