c7bf9b9f48bddb7d3380ebeb2028cd7a56919aad
Pete Woyzbun <peter.woyzbun@gmail.com>
2017-03-01 14:25:05 -0500

    Initializing docs.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import os
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
