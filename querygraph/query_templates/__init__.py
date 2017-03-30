790bbcd8d6c7a5100d652ec4e0b1b742f998c486
Pete Woyzbun <peter.woyzbun@gmail.com>
2017-03-30 17:17:53 -0400

    Removed QueryTemplate child classes because they are no longer needed! GoOdbye!

--- querygraph/query_templates/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from querygraph.query_templates.sqlite import SqliteTemplate as Sqlite
-from querygraph.query_templates.my_sql import MySqlTemplate as MySql
-from querygraph.query_templates.postgres import PostgresTemplate as Postgres
-from querygraph.query_templates.mongodb import MongoDbTemplate as MongoDb
-from querygraph.query_templates.elastic_search import ElasticSearchTemplate as ElasticSearch
-from querygraph.query_templates._cassandra import CassandraTemplate as Cassandra
